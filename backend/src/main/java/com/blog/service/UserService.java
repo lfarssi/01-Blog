80e7764b2690ecabcaced600d9b7b489af858075
lfarssi <medlfarssi10@gmail.com>
2026-01-02 17:20:55 +0100

    i forget where i was haha

--- backend/src/main/java/com/blog/service/UserService.java
+++ backend/src/main/java/com/blog/service/UserService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.blog.dto.UserResponse;
 @Service
-public interface UserService {
+public interface UserService  {
     UserResponse getUserProfile(String username);
     // UserResponse updateUserProfile(String username);
     // void banUser(long userId);
--- backend/src/main/java/com/blog/service/UserServiceImpl.java
+++ backend/src/main/java/com/blog/service/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.blog.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.blog.dto.UserResponse;
@@ -11,12 +14,18 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
     public UserResponse getUserProfile(String username){
         UserEntity user = userRepository.findByUsername(username).orElseThrow(()-> new RuntimeException("User not found"));
         return  UserMapper.toResponse(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
     }
 }
