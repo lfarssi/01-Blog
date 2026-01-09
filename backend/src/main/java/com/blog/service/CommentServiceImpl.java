8de6b4ca0a061cc909367793377ab64996e66c68
lfarssi <medlfarssi10@gmail.com>
2026-01-09 17:48:54 +0100

    notification for comments and likes

--- backend/src/main/java/com/blog/service/CommentServiceImpl.java
+++ backend/src/main/java/com/blog/service/CommentServiceImpl.java
@@ -10,79 +10,95 @@ import com.blog.dto.CommentRequest;
 import com.blog.dto.CommentResponse;
 import com.blog.entity.BlogEntity;
 import com.blog.entity.CommentEntity;
+import com.blog.entity.NotificationEntity;
 import com.blog.entity.UserEntity;
 import com.blog.mapper.CommentMapper;
 import com.blog.repository.BlogRepository;
 import com.blog.repository.CommentRepository;
+import com.blog.repository.NotificationRepository;
 import com.blog.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 
 public class CommentServiceImpl implements CommentService {
-    private final  CommentRepository commentRepository;
-    private  final UserRepository userRepository;
-    private  final BlogRepository blogRepository;
+        private final CommentRepository commentRepository;
+        private final UserRepository userRepository;
+        private final BlogRepository blogRepository;
+        private final NotificationRepository notificationRepository;
 
-    @Override
-    @Transactional
-    public CommentResponse createComment(Long blogId, CommentRequest request,String username){
-        UserEntity user=    userRepository.findByUsername(username)
-                        .orElseThrow(() -> new RuntimeException("User not found"));
-         BlogEntity blog = blogRepository.findById(blogId)
-                .orElseThrow(() -> new RuntimeException("Blog not found"));
-        CommentEntity comment = CommentEntity.builder()
-                .blog(blog)
-                .user(user)
-                .content(request.content())
-                .createdAt(Instant.now())
-                .updatedAt(Instant.now())
-                .build();
+        @Override
+        @Transactional
+        public CommentResponse createComment(Long blogId, CommentRequest request, String username) {
+                UserEntity user = userRepository.findByUsername(username)
+                                .orElseThrow(() -> new RuntimeException("User not found"));
+                BlogEntity blog = blogRepository.findById(blogId)
+                                .orElseThrow(() -> new RuntimeException("Blog not found"));
+                CommentEntity comment = CommentEntity.builder()
+                                .blog(blog)
+                                .user(user)
+                                .content(request.content())
+                                .createdAt(Instant.now())
+                                .updatedAt(Instant.now())
+                                .build();
 
-        comment = commentRepository.save(comment);
+                comment = commentRepository.save(comment);
 
-        blog.setComment_count(commentRepository.countByBlog_Id(blogId));
-        blogRepository.save(blog);
+                blog.setComment_count(commentRepository.countByBlog_Id(blogId));
+                blogRepository.save(blog);
 
-        return CommentMapper.toResponse(comment, user);
+                return CommentMapper.toResponse(comment, user);
 
-
-    }
-    
-    @Override
-    public List<CommentResponse> getCommentsByBlogId(Long blogId) {
-        List<CommentEntity> comments = commentRepository.findByBlog_Id(blogId);
-
-        return comments.stream()
-                .map(comment -> {
-                    UserEntity user = userRepository.findById(comment.getUser().getId())
-                            .orElseThrow(() -> new RuntimeException("User not found"));
-                    return CommentMapper.toResponse(comment, user);
-                })
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    @Transactional
-    public void deleteComment(Long commentId, String username) {
-        UserEntity user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        CommentEntity comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> new RuntimeException("Comment not found"));
-
-        if (!comment.getUser().equals(user.getId())) {
-            throw new RuntimeException("Unauthorized to delete this comment");
         }
 
-        BlogEntity blogId = comment.getBlog();
-        commentRepository.delete(comment);
+        @Override
+        public List<CommentResponse> getCommentsByBlogId(Long blogId) {
+                List<CommentEntity> comments = commentRepository.findByBlog_Id(blogId);
 
-        BlogEntity blog = blogRepository.findById(blogId.getId())
-                .orElseThrow(() -> new RuntimeException("Blog not found"));
-        blog.setComment_count(commentRepository.countByBlog_Id(blogId.getId()));
-        blogRepository.save(blog);
-    }
+                return comments.stream()
+                                .map(comment -> {
+                                        UserEntity user = userRepository.findById(comment.getUser().getId())
+                                                        .orElseThrow(() -> new RuntimeException("User not found"));
+                                        return CommentMapper.toResponse(comment, user);
+                                })
+                                .collect(Collectors.toList());
+        }
+
+        @Override
+        @Transactional
+        public void deleteComment(Long commentId, String username) {
+                UserEntity user = userRepository.findByUsername(username)
+                                .orElseThrow(() -> new RuntimeException("User not found"));
+
+                CommentEntity comment = commentRepository.findById(commentId)
+                                .orElseThrow(() -> new RuntimeException("Comment not found"));
+
+                if (!comment.getUser().equals(user.getId())) {
+                        throw new RuntimeException("Unauthorized to delete this comment");
+                }
+
+                Long blogId = comment.getBlog().getId();
+                commentRepository.delete(comment);
+
+                BlogEntity blog = blogRepository.findById(blogId)
+                                .orElseThrow(() -> new RuntimeException("Blog not found"));
+                blog.setComment_count(commentRepository.countByBlog_Id(blogId));
+                blogRepository.save(blog);
+                if (!blog.getUserId().getId().equals(user.getId())) {
+                        NotificationEntity notification = NotificationEntity.builder()
+                                        .user(blog.getUserId())
+                                        .type("COMMENT")
+                                        .content(user.getUsername() + " commented on your blog")
+                                        .relatedId(blogId)
+                                        .isRead(false)
+                                        .createdAt(Instant.now())
+                                        .updatedAt(Instant.now())
+                                        .build();
+                        notificationRepository.save(notification);
+                }
+
+        }
 }
